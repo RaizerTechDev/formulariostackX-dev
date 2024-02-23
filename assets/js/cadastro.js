e471f63ed098e13c228f64fb20c7fb20722d00e8
RAFARZ76 <rafaelraizer76@gmail.com>
2024-02-23 12:25:47 -0300

    update file.js

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -13,8 +13,8 @@
   // Aqui são definidos os endereços relevantes para a API e para as páginas de erro e sucesso.
   const adresses = {
     api: "https://api.sheetmonkey.io/form/uM1Q7XZyF85NbpeU6wSwFB",
-    errorPage: "https://error-cadastrodev-rafarz76dev.netlify.app/",
-    successPage: "https://sucess-cadastrodev-rafarz76dev.netlify.app/success_page.html",
+    errorPage: "https://rafarz76dev.github.io/formulariostackX-cadastro-dev/error_page.html",
+    successPage: "https://rafarz76dev.github.io/formulariostackX-cadastro-dev/sucess_page.html",
   };
 
   // Aqui são definidos objetos que representam o botão de envio do formulário, tanto em forma de imagem de carregamento como de mensagem.
