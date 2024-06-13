f8f3646ad5f09d08ab742828f01756b0615735f0
“RafaRz76Dev” <rafaelraizer76@gmail.com>
2024-06-12 22:58:08 -0300

    feat: Update folders and files

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -1,84 +1,119 @@
-(function () {
-  const htmlElements = {
-    formElement: document.querySelector("form"),
-    userName: document.querySelector("[js_data_name]"),
-    userTelephone: document.querySelector("[js_data_telephone]"),
-    userEmail: document.querySelector("[js_data_email]"),
-    userLanguage: document.querySelector("[js_data_language]"),
-    userMessage: document.querySelector("[js_data_message]"),
-    userExperience: document.querySelector("[js_data_experience]"),
-    buttonForm: document.querySelector("[js_data_form_button]"),
-  };
+class FormSubmit {
+  constructor(settings) {
+    this.settings = settings;
+    this.form = document.querySelector(settings.form);
+    this.formButton = document.querySelector(settings.button);
+    if (this.form) {
+      this.url = this.form.getAttribute("action");
+    }
+    this.sendForm = this.sendForm.bind(this);
+  }
 
-  // Aqui são definidos os endereços relevantes para a API e para as páginas de erro e sucesso.
-  const adresses = {
-    api: "https://api.sheetmonkey.io/form/uM1Q7XZyF85NbpeU6wSwFB",
-    errorPage:
-      "https://rafarz76dev-registration-formdev.netlify.app/error_page.html",
-    successPage:
-      "https://rafarz76dev-registration-formdev.netlify.app/success_page.html",
-  };
+  displaySuccess() {
+    // Redirecionar para uma nova página
+    window.location.href = this.settings.successRedirect;
+  }
 
-  // Aqui são definidos objetos que representam o botão de envio do formulário, tanto em forma de imagem de carregamento como de mensagem.
-  const renderButton = {
-    image:
-      '<img class="c-section-contact__button--animation" src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGo1MWEzNzJsdXRybjNoYm55aG1pcThsdnB0bDVtNWdxaWp0cnB4eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/CJFoawrEEq5P3ptexI/giphy.gif" alt="Loading">',
-    message: "SEND MESSAGE",
-  };
+  displayError() {
+    window.location.href = this.settings.errorRedirect;
+  }
 
-  const addLoad = () => {
-    htmlElements.buttonForm.innerHTML = renderButton.image;
-  };
+  getFormObject() {
+    const formObject = {};
+    const fields = this.form.querySelectorAll("[name]");
+    fields.forEach((field) => {
+      formObject[field.getAttribute("name")] = field.value;
+    });
+    return formObject;
+  }
 
-  const removedLoad = () => {
-    htmlElements.buttonForm.innerHTML = renderButton.message;
-  };
+  validateForm() {
+    const formObject = this.getFormObject();
+    let isValid = true;
+    let errorMessage = "";
 
-  const delaySuccess = () => {
-    setTimeout(() => {
-      window.location.href = adresses.successPage;
-      removedLoad();
-    }, 1000);
-  };
+    // Validação para o campo nome
+    if (!formObject.name || formObject.name.trim() === "") {
+      isValid = false;
+      errorMessage += "O campo Nome é obrigatório.\n";
+    }
 
-  const delayError = () => {
-    setTimeout(() => {
-      window.location.href = adresses.errorPage;
-      removedLoad();
-    }, 1000);
-  };
+    // Validação para o campo telefone (formato: (XX) XXXX-XXXX ou (XX) XXXXX-XXXX)
+    if (
+      !formObject.telephone ||
+      !/^\(\d{2}\) \d{4,5}-\d{4}$/.test(formObject.telephone)
+    ) {
+      isValid = false;
+      errorMessage +=
+        "O campo Telefone é obrigatório e deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX.\n";
+    }
 
-  const handleSubmit = (event) => {
+    // Validação para o campo email
+    if (
+      !formObject.email ||
+      !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formObject.email)
+    ) {
+      isValid = false;
+      errorMessage +=
+        "O campo Email é obrigatório e deve ser um email válido.\n";
+    }
+
+    // Validação para o campo mensagem adicional
+    if (!formObject.message || formObject.message.trim() === "") {
+      isValid = false;
+      errorMessage += "O campo Mensagem Adicional é obrigatório.\n";
+    }
+
+    if (!isValid) {
+      alert(errorMessage);
+    }
+
+    return isValid;
+  }
+
+  onSubmission(event) {
     event.preventDefault();
-    addLoad();
+    event.target.disabled = true;
+    event.target.innerHTML =
+      '<img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGo1MWEzNzJsdXRybjNoYm55aG1pcThsdnB0bDVtNWdxaWp0cnB4eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/CJFoawrEEq5P3ptexI/giphy.gif" alt="Enviando..." class="button--animation">';
+  }
 
-    const userNameValue = htmlElements.userName.value;
-    const userTelephoneValue = htmlElements.userTelephone.value;
-    const userEmailValue = htmlElements.userEmail.value;
-    const userLanguageValue = htmlElements.userLanguage.value;
-    const userExperienceValue = htmlElements.userExperience.value;
-    const userMessageValue = htmlElements.userMessage.value;
+  async sendForm(event) {
+    event.preventDefault(); // Adiciona preventDefault para evitar envio padrão
+    if (!this.validateForm()) {
+      event.target.disabled = false; // Reabilita o botão se a validação falhar
+      event.target.innerHTML = "SEND MESSAGE"; // Redefine o texto do botão se a validação falhar
+      return; // Interrompe o envio se o formulário for inválido
+    }
+    try {
+      this.onSubmission(event);
+      await fetch(this.url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(this.getFormObject()),
+      });
+      this.displaySuccess();
+    } catch (error) {
+      this.displayError();
+      throw new Error(error);
+    }
+  }
 
-    fetch(adresses.api, {
-      method: "post",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: userNameValue,
-        telephone: userTelephoneValue,
-        email: userEmailValue,
-        language: userLanguageValue,
-        experience: userExperienceValue,
-        message: userMessageValue,
-        date: new Date().toLocaleDateString(),
-        time: new Date().toLocaleTimeString(),
-      }),
-    })
-      .then(() => delaySuccess())
-      .catch(() => delayError());
-  };
+  init() {
+    if (this.form) this.formButton.addEventListener("click", this.sendForm);
+    return this;
+  }
+}
 
-  htmlElements.formElement.addEventListener("submit", handleSubmit);
-})();
+const formSubmit = new FormSubmit({
+  form: "[data-form]",
+  button: "[data-button]",
+  successRedirect:
+    "https://rafarz76dev-registration-formdev.netlify.app/success_page.html", // Página para redirecionar após sucesso
+  errorRedirect:
+    "https://rafarz76dev-registration-formdev.netlify.app/error_page.html", // Página para redirecionar após erro
+});
+formSubmit.init();
