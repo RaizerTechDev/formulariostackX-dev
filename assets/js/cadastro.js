58586b772c3636cceb5794856682c46493caa2f9
“RafaRz76Dev” <rafaelraizer76@gmail.com>
2024-06-13 12:27:24 -0300

    feat: Update  assets/js/cadastro.js

--- assets/js/cadastro.js
+++ assets/js/cadastro.js
@@ -10,7 +10,6 @@ class FormSubmit {
   }
 
   displaySuccess() {
-    // Redirecionar para uma nova página
     window.location.href = this.settings.successRedirect;
   }
 
@@ -30,38 +29,42 @@ class FormSubmit {
   validateForm() {
     const formObject = this.getFormObject();
     let isValid = true;
-    let errorMessage = "";
+    let errorMessage = '';
 
     // Validação para o campo nome
-    if (!formObject.name || formObject.name.trim() === "") {
+    if (!formObject.name || formObject.name.trim() === '') {
       isValid = false;
-      errorMessage += "O campo Nome é obrigatório.\n";
+      errorMessage += 'Campo Nome é obrigatório.\n';
     }
 
-    // Validação para o campo telefone (formato: (XX) XXXX-XXXX ou (XX) XXXXX-XXXX)
-    if (
-      !formObject.telephone ||
-      !/^\(\d{2}\) \d{4,5}-\d{4}$/.test(formObject.telephone)
-    ) {
+    // Validação para o campo telefone (formato: (XX) XXXXX-XXXX)
+    if (!formObject.telephone || !/^\(\d{2}\) \d{5}-\d{4}$/.test(formObject.telephone)) {
       isValid = false;
-      errorMessage +=
-        "O campo Telefone é obrigatório e deve estar no formato (XX) XXXX-XXXX ou (XX) XXXXX-XXXX.\n";
+      errorMessage += 'Campo Telefone é obrigatório e deve estar no formato (XX) XXXXX-XXXX.\n';
     }
 
     // Validação para o campo email
-    if (
-      !formObject.email ||
-      !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formObject.email)
-    ) {
+    if (!formObject.email || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formObject.email)) {
       isValid = false;
-      errorMessage +=
-        "O campo Email é obrigatório e deve ser um email válido.\n";
+      errorMessage += 'Campo Email é obrigatório e deve ser um email válido.\n';
+    }
+
+    // Validação para o campo linguagem
+    if (!formObject.language || formObject.language.trim() === '') {
+      isValid = false;
+      errorMessage += 'Campo Linguagem é obrigatório.\n';
+    }
+
+    // Validação para o campo experiência
+    if (!formObject.experience || isNaN(formObject.experience)) {
+      isValid = false;
+      errorMessage += 'Campo Experiência é obrigatório e deve ser um número válido.\n';
     }
 
     // Validação para o campo mensagem adicional
-    if (!formObject.message || formObject.message.trim() === "") {
+    if (!formObject.message || formObject.message.trim() === '') {
       isValid = false;
-      errorMessage += "O campo Mensagem Adicional é obrigatório.\n";
+      errorMessage += 'Campo Mensagem Adicional é obrigatório.\n';
     }
 
     if (!isValid) {
@@ -74,15 +77,14 @@ class FormSubmit {
   onSubmission(event) {
     event.preventDefault();
     event.target.disabled = true;
-    event.target.innerHTML =
-      '<img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGo1MWEzNzJsdXRybjNoYm55aG1pcThsdnB0bDVtNWdxaWp0cnB4eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/CJFoawrEEq5P3ptexI/giphy.gif" alt="Enviando..." class="button--animation">';
+    event.target.innerHTML = '<img src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExeGo1MWEzNzJsdXRybjNoYm55aG1pcThsdnB0bDVtNWdxaWp0cnB4eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/CJFoawrEEq5P3ptexI/giphy.gif" alt="Enviando..." class="button--animation">';
   }
 
   async sendForm(event) {
-    event.preventDefault(); // Adiciona preventDefault para evitar envio padrão
+    event.preventDefault();
     if (!this.validateForm()) {
       event.target.disabled = false; // Reabilita o botão se a validação falhar
-      event.target.innerHTML = "SEND MESSAGE"; // Redefine o texto do botão se a validação falhar
+      event.target.innerHTML = 'SEND MESSAGE'; // Redefine o texto do botão se a validação falhar
       return; // Interrompe o envio se o formulário for inválido
     }
     try {
@@ -111,9 +113,7 @@ class FormSubmit {
 const formSubmit = new FormSubmit({
   form: "[data-form]",
   button: "[data-button]",
-  successRedirect:
-    "https://rafarz76dev-registration-formdev.netlify.app/success_page.html", // Página para redirecionar após sucesso
-  errorRedirect:
-    "https://rafarz76dev-registration-formdev.netlify.app/error_page.html", // Página para redirecionar após erro
+  successRedirect: "https://rafarz76dev-registration-formdev.netlify.app/success_page.html",
+  errorRedirect: "https://rafarz76dev-registration-formdev.netlify.app/error_page.html",
 });
 formSubmit.init();
